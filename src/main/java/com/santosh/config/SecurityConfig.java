3a8f2609682654aaf80a945e2374924787e36835
santosh sah <sk.santosh99111@gmail.com>
2020-08-05 12:40:24 +0530

    demonstrated JDBC authentication with embedded H2 and then  using postgres

--- src/main/java/com/santosh/config/SecurityConfig.java
+++ src/main/java/com/santosh/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.santosh.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,11 +14,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("santosh").password("iamsantosh").roles("ADMIN").and()
-		.withUser("kumar").password("kumar").roles("USER");
 		
+		/* In memory authentication
+		 * auth.inMemoryAuthentication().withUser("santosh").password("iamsantosh").
+		 * roles("ADMIN").and() .withUser("kumar").password("kumar").roles("USER");
+		 */
+		
+		//JDBC authentication for inbuild H2 database
+		/*
+		 * auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema() //
+		 * create user table and authority table
+		 * .withUser(User.withUsername("santosh").password("iamsantosh").roles("USER"))
+		 * .withUser(User.withUsername("kumar").password("iamsantosh").roles("ADMIN"));
+		 */
+		
+		// with your own database
+		auth.jdbcAuthentication().dataSource(dataSource)
+		.usersByUsernameQuery("select username, password, enabled "+ "from users "+ "where username = ?")
+		.authoritiesByUsernameQuery("select username, authority as role "+" from authorities "+"where username= ?");
 	}
 	
 	@Bean
